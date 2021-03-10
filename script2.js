17730b638d720cc0525328f4ab8b918f5454fa73
demilii <44440943+demilii@users.noreply.github.com>
2021-03-10 16:43:46 +0800

    Add files via upload

--- script2.js
+++ script2.js
@@ -12,10 +12,10 @@ function check() {
     } else {
         if (document.getElementById("feedback").getElementsByTagName("P")[0] == undefined) {
             document.getElementById("feedback").appendChild(p);
-            document.getElementById("feedback").getElementsByTagName("P")[0].innerText = "Use the simple interest formula with given initial principal, interest rate and time.";
+            document.getElementById("feedback").getElementsByTagName("P")[0].innerText = "Well done! Jack will get $1000 back after 5 years with simple interest. Remember the formula of the simple interest is I = prt. In this case, p(investment) = $20,000, r(interest rate) = 10%, t = 5, so the answer should be I = 20,000*0.1*5 = $1000.";
             document.getElementById("feedback").getElementsByTagName("P")[0].style.padding = "20px";
         } else {
-            document.getElementById("feedback").getElementsByTagName("P")[0].innerText = "Use the simple interest formula with given initial principal, interest rate and time.";
+            document.getElementById("feedback").getElementsByTagName("P")[0].innerText = "Well done! Jack will get $1000 back after 5 years with simple interest. Remember the formula of the simple interest is I = prt. In this case, p(investment) = $20,000, r(interest rate) = 10%, t = 5, so the answer should be I = 20,000*0.1*5 = $1000.";
         }
     }
 }
