a44d061e9c18dbb62262eea66814fe6f1fa18df5
demilii <44440943+demilii@users.noreply.github.com>
2021-03-10 10:55:18 +0800

    fourth commit

--- script5.js
+++ script5.js
@@ -10,29 +10,23 @@ function compound_interest(n) {
 function simple_interest(n) {
     let principal = 1000;
     principal += principal * 0.12 * n;
-    return principal.toFixed(2);
+    return principal.toFixed(0);
+}
+let data = [];
+function create_data(n) {
+    data = [];
+    for (let i = 0; i <= n; i++) {
+        data.push({ time: i, jones: simple_interest(i), wendi: compound_interest(i) })
+    }
 }
 
-// create the dataset to visualize
-let ydata0, ydata1, ydata2, ydata3, ydata4, ydata5, ydata6, ydata7;
-y_data0 = compound_interest(0);
-y_data1 = compound_interest(1);
-y_data2 = compound_interest(2);
-y_data3 = compound_interest(3);
-y_data4 = simple_interest(0);
-y_data5 = simple_interest(1);
-y_data6 = simple_interest(2);
-y_data7 = simple_interest(3);
-let data = [{ time: 0, jones: y_data4, wendi: y_data0 },
-{ time: 1, jones: y_data5, wendi: y_data1 },
-{ time: 2, jones: y_data6, wendi: y_data2 },
-{ time: 3, jones: y_data7, wendi: y_data3 }
-]
 
+
+// create_data(15);
 
 //data visualization of the chart
 let w = 800;
-let h = 400;
+let h = 600;
 let padding = 50;
 
 let viz = d3.select("#charts")
@@ -41,72 +35,87 @@ let viz = d3.select("#charts")
     .style("height", h)
     .style("display", "block")
     .style("margin", "auto"); //center the vis
-viz.append("text")
+
+
+// change(data);
+change_year();
+document.getElementById("myRange").addEventListener("change", change_year);
+function change_year() {
+    let val = document.getElementById("myRange").value;
+    document.getElementById("val").getElementsByTagName("P")[0].innerText = "time: " + document.getElementById("myRange").value +" years";
+    create_data(val);
+    change(data);
+}
+function change(data) {
+    viz.select(".yAxis").remove();
+    viz.select(".xAxis").remove();
+    viz.selectAll("rect").remove();
+    viz.selectAll("text").remove();
+    viz.append("text")
     .attr("text-align", "center")
     .attr("font-size", "15px")
     .attr("transform", "translate(300,15)")
     .text("Mrs. Jones' & Wendi's Savings");
-let allNames = data.map(function (d) { return d.time; });
-let xScale = d3.scaleBand()
-    .domain(allNames)
-    .range([2 * padding, w - 2 * padding])
-    .paddingInner(0.3)
-    ;
-// create a visual axis corresponding to the scale.
-// reference:https://www.d3-graph-gallery.com/graph/barplot_grouped_basicWide.html
-let xAxis = d3.axisBottom(xScale);
-let xAxisGroup = viz.append("g")
-    .classed("xAxis", true);
-xAxisGroup.call(xAxis)
-    .append("text")
-    .attr("x", w - padding * 2)
-    .attr("dy", "3em")
-    .style("text-anchor", "end")
-    .text("Time / year")
-    .attr("fill", "black");
-// xAxisGroup.text("Time");
-xAxisGroup.selectAll("text")
-    .attr("font-size", 12);
-xAxisGroup.attr("transform", "translate(0," + (h - padding) + ")");
+    let allNames = data.map(function (d) { return d.time; });
+    let xScale = d3.scaleBand()
+        .domain(allNames)
+        .range([2 * padding, w - 2 * padding])
+        .paddingInner(0.3)
+        ;
+    // create a visual axis corresponding to the scale.
+    // reference:https://www.d3-graph-gallery.com/graph/barplot_grouped_basicWide.html
+    let xAxis = d3.axisBottom(xScale);
+    let xAxisGroup = viz.append("g")
+        .classed("xAxis", true);
+    xAxisGroup.call(xAxis)
+        .append("text")
+        .attr("x", w - padding * 2)
+        .attr("dy", "3em")
+        .style("text-anchor", "end")
+        .text("Time / year")
+        .attr("fill", "black");
+    // xAxisGroup.text("Time");
+    xAxisGroup.selectAll("text")
+        .attr("font-size", 12);
+    xAxisGroup.attr("transform", "translate(0," + (h - padding) + ")");
+
+    // let yMax = Math.max(data);
+    // let yMin = Math.min(data)
+    let yMax = d3.max(data, function (d) { return (d.wendi); });
+    let yMin = d3.min(data, function (d) { return (d.wendi); });
 
 
-let yMax = d3.max(data, function (d) { return d.savings; });
-let yMin = d3.min(data, function (d) { return d.savings; });
+    yDomain = [yMin / 2, yMax + 100];
+    let yScale = d3.scaleLinear()
+        .domain([yMin / 2, yMax])
+        .range([h - padding * 2, 0]);
 
-yDomain = [yMin / 2, yMax + 100];
-let yScale = d3.scaleLinear()
-    .domain([500, 1500])
-    .range([h - padding * 2, 0]);
+    let yAxis = d3.axisLeft(yScale);
+    let yAxisGroup = viz.append("g")
+        .classed("yAxis", true);
+    yAxisGroup.call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "-5em")
+        .style("text-anchor", "end")
+        .text("Savings / $")
+        .attr("fill", "black");
 
-let yAxis = d3.axisLeft(yScale);
-let yAxisGroup = viz.append("g")
-    .classed("yAxis", true);
-yAxisGroup.call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", "-5em")
-    .style("text-anchor", "end")
-    .text("Savings / $")
-    .attr("fill", "black");
+    yAxisGroup.selectAll("text")
+        .attr("font-size", 12);
+    yAxisGroup.attr("transform", "translate(" + 2 * padding + "," + padding + ")");
 
-yAxisGroup.selectAll("text")
-    .attr("font-size", 12);
-yAxisGroup.attr("transform", "translate(" + 2 * padding + "," + padding + ")");
+    // another scale for subgroup position
+    var xSubgroup = d3.scaleBand()
+        .domain(['jones', 'wendi'])
+        .range([0, xScale.bandwidth()])
+        .padding([0.01]);
 
-// another scale for subgroup position
-var xSubgroup = d3.scaleBand()
-    .domain(['jones', 'wendi'])
-    .range([0, xScale.bandwidth()])
-    .padding([0.01]);
-
-var color = d3.scaleOrdinal()
-    .domain(['jones', 'wendi'])
-    .range(['#377eb8', '#4daf4a']);
-
-change(data);
-
-function change(data) {
+    var color = d3.scaleOrdinal()
+        .domain(['jones', 'wendi'])
+        .range(['#377eb8', '#4daf4a']);
+   
     viz.append("g")
         .selectAll("g")
         .data(data)
@@ -120,7 +129,7 @@ function change(data) {
         .attr("y", function (d, i) {
             // ...and then push the bar up since it
             // is drawn from top to bottom
-            // console.log(d);
+            // console.log(d.key);
             return -h + padding * 2 + yScale(d.value);
         })
         .attr("width", xSubgroup.bandwidth())
@@ -128,23 +137,23 @@ function change(data) {
         .attr("fill", function (d) { return color(d.key); });
 
     viz.append("circle")
-        .attr("cx", 710)
+        .attr("cx", 120)
         .attr("cy", 40)
         .attr("r", 3)
         .style("fill", "#377eb8")
     viz.append("circle")
-        .attr("cx", 710)
+        .attr("cx", 120)
         .attr("cy", 60)
         .attr("r", 3)
         .style("fill", '#4daf4a')
     viz.append("text")
-        .attr("x", 720)
+        .attr("x", 130)
         .attr("y", 40)
         .text("Jones")
         .style("font-size", "10px")
         .attr("alignment-baseline", "middle")
     viz.append("text")
-        .attr("x", 720)
+        .attr("x", 130)
         .attr("y", 60)
         .text("Wendi")
         .style("font-size", "10px")
@@ -159,18 +168,23 @@ function change(data) {
         .selectAll("text")
         .data(function (d) { return ['jones', 'wendi'].map(function (key) { return { key: key, value: d[key] }; }); })
         .enter().append("text")
-        .attr("x", function (d) { console.log(xSubgroup(d.key)); return xSubgroup(d.key); })
+        .attr("x", function (d) { return xSubgroup(d.key); })
         .attr("y", function (d, i) {
             // ...and then push the bar up since it
             // is drawn from top to bottom
             // console.log(d);
             // console.log(-h + padding * 2 + yScale(d.value))
-            return -h + padding * 2 + yScale(d.value) - 5;
+            if (d.key == "jones") {
+                return -h + padding * 2 + yScale(d.value) + 10;
+            } else if (d.key == "wendi") {
+                return -h + padding * 2 + yScale(d.value) - 5;
+            }
+
         })
         .text(function (d) {
             return '$' + d.value;  // Value of the text
         })
-        .attr("font-size", 12)
+        .attr("font-size", 7)
         .attr("fill", "black");
 }
 
